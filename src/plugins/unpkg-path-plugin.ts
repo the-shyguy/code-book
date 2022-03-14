4d7bad242bc9d48799663897a5a8263dbfdaeb04
Rohit Singh <rohit14kvs@gmail.com>
2022-03-14 22:10:54 +0530

    Fixed bundling warnings

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -34,7 +34,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('nested-test-pkg');
+              const message = require('lodash');
               console.log(message);
             `,
           };
